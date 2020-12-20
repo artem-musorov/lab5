b9bd66a373be3b5c8febd4db5cc648d16ecab367
aksenia-studen <76278453+aksenia-studen@users.noreply.github.com>
2020-12-20 10:50:56 +0200

    Update User.cs
    Added additional fields

--- User.cs
+++ User.cs
@@ -9,5 +9,9 @@ namespace lab10.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string Age { get; set; }
+        public string City { get; set; }
     }
 }
